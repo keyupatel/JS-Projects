32ed16f98522bd7b6d4d0bc2dd9d171c47b13566
KeyurPatel <keyupatel007@gmail.com>
2025-09-18 07:50:25 +0530

    Datatype conversion

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -1,2 +1,26 @@
 "use strict" ; // Treat all js code as newer version
-console.log(typeof "Keyur");
+// console.log(typeof "Keyur");
+
+// Data types conversions
+
+let score = 33;
+//console.log(typeof score); 
+let age = "33a";
+let valueInnumber = Number(age);  // Convert in number
+console.log(typeof age);
+console.log(typeof valueInnumber); // convert in number o/p
+console.log(valueInnumber); // NaN Not a Number o/p
+let gender = true;
+let genderInNumber = Number (gender);
+console.log(genderInNumber); // Value return 1 
+
+// convert into Boolean
+let isLoggenIn = "hitesh";
+let booleanLoggedIn =  Boolean (isLoggenIn);
+console.log (booleanLoggedIn);
+
+// convert into String
+
+let name = 33;
+let result = String (name);
+console.log(typeof (result));
