19cb8c366cc4417a36b6bb7575c35b2c1983d395
KeyurPatel <keyupatel007@gmail.com>
2025-09-18 09:34:47 +0530

    Compariosons Operators

---
+++ 04_comparisons.js
@@ -0,0 +1,22 @@
+// Comparison of datatypes
+/*
+console.log( 2 > 0);  // o/p: true
+console.log( 2 < 0);  // o/p: false
+
+console.log(null > 0); // o/p: false
+console.log(null >= 0); // O/p: true
+console.log(null = 0);  // false
+
+*/
+
+// Equal to  (==), (===), (!=), (!==)
+console.log(5 == 5); // true => value check thase not datatype
+console.log(5 == "5"); // true => value check thase not datatype
+console.log(5 === "5"); // false => value & Datatype check thase
+console.log(5 === 5); // true => value & Datatype check thase
+
+console.log (5 != 5); // false bane equal chhe datatype check thase
+console.log (5 != 3); // true bane equal chhe datatype check thase
+
+console.log (5 !== 5); // false bane equal chhe 
+console.log (5 !== "5"); // true bane equal chhe datatype check thase
