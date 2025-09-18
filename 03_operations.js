d0ad56055a9ec73c7a5cf6da72f6003dfbd539a3
KeyurPatel <keyupatel007@gmail.com>
2025-09-18 08:48:00 +0530

    Operaters and Prefix & Postfix

---
+++ 03_operations.js
@@ -0,0 +1,33 @@
+console.log(3 + 3); //o/p: 6
+console.log(3 - 3); //o/p: 0
+console.log(3 * 3); //o/p: 9
+console.log(3 / 3); //o/p: 1
+console.log (3 % 3); //o/p: 0;
+
+console.log ("3" + 1); //"3" is string value so o/p 31
+console.log ("3" + 1 + 1) // o/p: 311 string pachhi value concate thase operations add nai thase
+console.log (1 + 1 + "3") // o/p: 23 string pehla ni value ma operation perform thase
+console.log (1 + 1 + "3" + 1) // o/p: 231 string pehla ni value ma operation perform thase
+
+
+let str1= "Keyur";
+let str2 = " Patel";
+let str3 = str1 + str2;
+console.log(str3);
+
+// Prefix and Postfix (++, --)
+// eg of perfix aama pehla thi j value increment thai jai pachhi assign thai chhe
+
+let a = 5;
+let b = ++a;
+console.log(a);
+console.log(b);
+
+let c = 4;
+let d = c++;
+console.log(c);
+console.log(d);
+
+
+
+
